567679d291592eaf288f59d4fad7563a188edd21
Neerajkumhar <webassistantsnotes@gmail.com>
2026-09-07 09:51:38 +0530

    Add final CTA section and compose home page in narrative order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ import { ScalingSection } from "@/components/sections/home/ScalingSection";
 import { ApplicationsSection } from "@/components/sections/home/ApplicationsSection";
 import { ProofSection } from "@/components/sections/home/ProofSection";
 import { PartnerSection } from "@/components/sections/home/PartnerSection";
+import { FinalCtaSection } from "@/components/sections/home/FinalCtaSection";
+import { Divider } from "@/components/ui/Divider";
 
 export default function Home() {
   return (
     <main id="main">
       <Hero />
+      <Divider className="container-content" />
       <ProblemSection />
       <ArchitectureSection />
+      <Divider className="container-content" />
       <ScalingSection />
       <ApplicationsSection />
+      <Divider className="container-content" />
       <ProofSection />
       <PartnerSection />
+      <FinalCtaSection />
     </main>
   );
 }
